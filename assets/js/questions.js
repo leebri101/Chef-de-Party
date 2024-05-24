4c70689a18fd24cc5f02f15a9fb39eab85230352
Brian Lee <brian-lee@live.co.uk>
2024-05-24 14:34:42 +0100

    Initial commit of question script

---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+
+/**
+ * Question and Answer structure:
+ * Each Q&A object is set up as an array,
+ * Each of the objects contain:
+ * 1. Question numbers for the user display in the #question-number element in quiz-tracker. 
+ * (This is a requirement as the indexes start from 0).
+ * Unintuitive for casual users or with little to no knowledge.
+ * 2. Each question text will fill the #question-box element.
+ * 3. Each object contains an array with 4 texted based choices to pick from.
+ * 4. The object which contains the correct answer will be compared from the user's selection from 'choices'.
+ */
