ad8bd5f3f67a782fb6bd2f1074374a39ba1fc0c3
Brian Lee <brian-lee@live.co.uk>
2024-05-24 14:37:07 +0100

    Update of comments in question script

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,9 +4,9 @@
  * Each Q&A object is set up as an array,
  * Each of the objects contain:
  * 1. Question numbers for the user display in the #question-number element in quiz-tracker. 
+ * 2. Each question text will fill the #question-box element.
+ * 3. Each object contains an array with 4 texted based choices for the user to pick form.
+ * 4. The object which contains the correct answer will be compared from the user's selection of 'choices'.
  * (This is a requirement as the indexes start from 0).
  * Unintuitive for casual users or with little to no knowledge.
- * 2. Each question text will fill the #question-box element.
- * 3. Each object contains an array with 4 texted based choices to pick from.
- * 4. The object which contains the correct answer will be compared from the user's selection from 'choices'.
  */
