6a83efae252a7d0864df10f83670eaff35ef2408
Brian Lee <brian-lee@live.co.uk>
2024-06-17 15:18:11 +0100

    Comments on fuctionality added into script

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,15 @@ const playerName = document.getElementById("player-name");
 const startBtn = document.getElementById("start-btn");
 const quitBtn = document.getElementById("quit-btn");
 
+
+
+/**
+ * The CSS styling elements that are used in the script is to hide sections of the site for a cleaner/smoother look.
+ * Except for the home-page which has a Call To Action (CTA) play button, which prompts the user click to start the quiz.
+ * When clicking the CTA button it displays a text-box to tell the user to enter a name within the placeholder box,
+ * which players are instructed to enter their name and click the start button to begin the quiz.
+ * Players are also given the option to quit the quiz at any time by clicking the quit button. 
+ */
 function startNewGame() {
     landingSection.style.display = "none";
     newGameSection.style.display = "inline-flex";
